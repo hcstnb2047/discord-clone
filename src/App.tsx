4c06e06e374c1924650a5cb348d0b7b781497b5b
tanabe <yts2019hc2047@gmail.com>
2025-05-31 15:18:01 +0900

    feat: add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='App'>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className='sidebarLeft'>
+            <div className="serverIcon">
+                <img src="./logo192.png" alt="serverIcon" />
+            </div>
+            <div className="serverIcon">
+                <img src="./logo192.png" alt="serverIcon" />
+            </div>
+
+
+
+        </div>
+        <div className='sidebarRight'>
+            <div className="sidebarTop">
+                <h3>Discord</h3>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
